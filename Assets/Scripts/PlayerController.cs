3dcc573895673e268ad9d1debb12446841f46ec5
Gabriel <gabriel.candido@id.uff.br>
2023-02-04 19:34:19 -0300

    Anim Básicas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 2f;
+    public float previousHorizontal = 0;
     private Rigidbody2D rb;
     public Animator animator;
     public int diggingPower = 10;
-
+    public bool facingRight = true;
+    public bool facingLeft = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        previousHorizontal = Input.GetAxis("Horizontal");
     }
 
     void Update()
@@ -25,11 +28,22 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Horizontal", horizontal);
         animator.SetFloat("Vertical", vertical);
         animator.SetFloat("Speed", Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)){
+            animator.SetBool("isWalking", true);
+        }else{
+            animator.SetBool("isWalking", false);
+        }
 
-
-
+        if(previousHorizontal < horizontal){
+            animator.SetBool("facingLeft", false);
+            animator.SetBool("facingRight", true);
+        }else if(previousHorizontal >  horizontal){
+            animator.SetBool("facingRight", false);
+            animator.SetBool("facingLeft", true);
+        }
+        previousHorizontal = Input.GetAxis("Horizontal");
 
        
     }
-
+    
 }
