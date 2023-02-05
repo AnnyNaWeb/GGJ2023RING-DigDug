054749a3fb2639698929ee87bc3c242fc5194717
annynaweb <sousa.annyc@gmail.com>
2023-02-05 08:29:36 -0300

    inimigo que segue o player funcional

--- Assets/Prefabs/Block.cs
+++ Assets/Prefabs/Block.cs
@@ -4,19 +4,5 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _break;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-
-     //  Instantiate(_break, transform.position, Quaternion.identity);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
