8d53093ba5eda8f48d337cc85dd873e75d6a403e
annynaweb <sousa.annyc@gmail.com>
2023-02-04 22:17:48 -0300

    rezando

---
+++ Assets/Scripts/Digger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Digger : MonoBehaviour
+{
+    public int diggingPower = 10;
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TerrainDig.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainDig : MonoBehaviour
+{
+    [SerializeField]
+    private Transform diggingPoint;
+
+    [SerializeField]
+    private float diggingRadius = 0.5f;
+
+    [SerializeField]
+    private LayerMask terrainLayer;
+
+
+    private void Update()
+    {
+      
+
+       
+    }
+
+
+
+
+}
